9e25dc1c2a4922c4585dec7f7efa7d385baba09f
Ana Lorenzo <ana.lorenzo77@hotmail.com>
2025-11-25 16:42:37 -0600

    adding maps.html & css + script js + added navigation for the three pages

---
+++ script.js
@@ -0,0 +1,32 @@
+function initMap() {
+  const allianzArena = { lat: 48.2188, lng: 11.6247 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: allianzArena,
+    mapTypeId: "terrain"
+  });
+
+  const marker = new google.maps.Marker({
+    position: allianzArena,
+    map: map,
+    title: "Allianz Arena (Home of FC Bayern Munich)"
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "Allianz Arena - Home stadium of FC Bayern Munich"
+  });
+
+  marker.addListener("click", function () {
+    infoWindow.open(map, marker);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const questionsButton = document.getElementById("questions");
+  if (questionsButton) {
+    questionsButton.addEventListener("click", function () {
+      alert("If you have questions, contact me at:\nalorenzo1@hawk.iit.edu");
+    });
+  }
+});
